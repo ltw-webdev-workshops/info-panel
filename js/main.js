2878eebf80da0255b1646783f549216c0a5abc85
Thomas J Bradley <hey@thomasjbradley.ca>
2019-03-19 14:50:52 -0400

    Add all the starter code to the repo

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/*
+MIXED UP LINES OF CODE
+============================
+
+if (!isOpen) {
+
+$(this).addClass('info-panel-btn-active');
+
+$('.info-panel').attr('hidden', true);
+
+$('.info-panel-btn').on('click', function () {
+
+$(id).removeAttr('hidden');
+
+var isOpen = $(this).hasClass('info-panel-btn-active');
+
+$('.info-panel-btn').removeClass('info-panel-btn-active');
+
+var id = $(this).attr('href');
+
+}
+
+});
+
+*/
